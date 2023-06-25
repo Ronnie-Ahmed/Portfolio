eea5acb80733ca9d6551544ca734411a610f0700
Ronnie-Ahmed <rksraisul@gmail.com>
2023-06-25 08:07:16 +0600

    fixed

--- src/Pages/Contractme.js
+++ src/Pages/Contractme.js
@@ -103,6 +103,7 @@ export const Contractme = () => {
               required
             ></textarea>
           </div>
+
           <button
             type="submit"
             className="px-6 py-3 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-700 transition-colors shadow-lg hover:shadow-xl"
